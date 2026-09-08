75da5a91e06611bc84edf3e703b56fd6bc2abf3c
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-09-07 18:30:02 -0700

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,8 @@
+var = 10 #initialize
+while var > 0: #iterate the loop
+    var = var - 1
+    if var == 5: #condition 1
+        continue #continue the statement
+    #display the result
+    print("\nCurrent variable value:", var)
+print("\nGoodbye!")
