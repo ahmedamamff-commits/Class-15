221bca6b493e51a872b4743ead676f8e0305fe0c
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-09-07 18:24:27 -0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,15 @@
+for x in range(10): #iterate the loop
+    if x % 20 == 0: #condition 1
+        print("twist")
+
+    elif x % 15 == 0: #condition 2
+        pass #pass statement
+
+    elif x % 5 == 0: #condition 3
+        print("fizz") #display the result
+
+    elif x % 3 == 0: #condition 4
+        print("buzz") #display the result
+
+    else: #condition 5
+        print(x) #display the result
